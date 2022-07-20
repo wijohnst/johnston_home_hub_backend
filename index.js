222f4f59d4d56e9ff8e9ad799a353ad703db629a
will-johnston-goodyear <will_johnston@goodyear.com>
2022-07-20 11:24:00 -0400

    Adds mongodb connection

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+require('dotenv').config();
+
+const mongoString = process.env.DATABASE_URL;
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = 3001;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
+mongoose.connect(mongoString);
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+	console.log('DB Connection Error', error);
 })
+
+database.once('connected', () => { 
+	console.log('Successfully connected to MongoDB...')
+})
+
+app.use(express.json());
 
 app.listen(port, () => {
 	console.log(`Johnston Home Hub is listening on port ${port}`);
