1d07009316cc243e4ae2805df6b14e3a073fd8e4
will-johnston-goodyear <will_johnston@goodyear.com>
2022-07-26 15:45:01 -0400

    Adds petch endpoint

--- contants.js
+++ contants.js
@@ -1,6 +1,7 @@
 const SuccessMessagesEnum = Object.freeze({
 	PETS_FETCHED: 'Pets fetched successfully',
-	FEEDER_DATA_FETCHED: 'Feeder data fetched successfully'
+	FEEDER_DATA_FETCHED: 'Feeder data fetched successfully',
+	FEEDER_DATA_PATHED: 'Feeder data patched successfully',
 });
 
 module.exports = [ SuccessMessagesEnum ];
