79429906885fbc269ce270f855488cf7b723e289
will-johnston-goodyear <will_johnston@goodyear.com>
2022-08-23 13:47:24 -0400

    Adds jests and recipe tests

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const config = {
+  testEnvironment: 'node',
+	verbose: true,
+};
+
+module.exports = config;
--- routes/Recipe/recipe.route.js
+++ routes/Recipe/recipe.route.js
@@ -2,39 +2,51 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const pretty = require('pretty');
+const { getTargetSelectorFromUrl } = require('./recipe.route.utils');
+const { HTTPMessagesEnum } = require('../../constants');
 
 const router = express.Router();
 
-router.get('/generate', async (req, res) => {
+router.post('/generate', async (req, res) => {
 	console.log('Generating requested recipe...');
+	const { url } = req.body;
 
-	const url = "https://www.allrecipes.com/recipe/20724/grilled-corn-on-the-cob/";
 	try{
 
+		const targetSelectors = getTargetSelectorFromUrl(url);	
+		
 		const { data } = await axios(url);	
 		const $ = cheerio.load(data);
 	
-		const targetSelectors = {
-			targetParentClass: ".ingredients-section",
-			targetDomElementClass: ".ingredients-item-name",
-		};
-
-		const ingredientsSection = $(targetSelectors.targetParentClass);
+		
+		const ingredientsSection = $(targetSelectors.targetParentClass.ingredients);
 		const ingredients = [];
 
 		/*
 			Finds each target DOM element in the section and returns that targets text content, trimming whitespace for each string
 		*/
-		$(ingredientsSection.find($(targetSelectors.targetDomElementClass))).each((_index, element) => ingredients.push($(element).text().trim()));
+		$(ingredientsSection.find($(targetSelectors.targetDomElementClass.ingredient))).each((_index, element) => ingredients.push($(element).text().trim()));
+
+		const stepsSection = $(targetSelectors.targetParentClass.steps);
+		const steps = [];
+
+		$(stepsSection.find($(targetSelectors.targetDomElementClass.step))).each((_index, element) => steps.push($(element).text().trim()));
 
 		res.status(200).json({
 			status: 200,
-			message: "Bingo. "
+			message: HTTPMessagesEnum.GENERATE_RECIPE.SUCCESS,
+			recipe: {
+				ingredients,
+				steps
+			}
 		})
 
 	}catch(error){
-		console.error(error)
+		console.error(error);
+		res.status(400).json({
+			status: 400,
+			message: error,
+		})
 	}
 });
 
