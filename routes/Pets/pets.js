ca73c2395cc2070c39460093d5f303e1e09a7173
Will Johnston <will.johnston@kroger.com>
2023-10-24 20:29:15 -0700

    debug

--- routes/Pets/pets.js
+++ routes/Pets/pets.js
@@ -5,22 +5,27 @@ const { Pet } = require('../../models/Pet/Pet')
 const {HTTPMessagesEnum} = require('../../constants')
 
 router.get('/all', async (req, res) => {
-    console.log('Fetching pets from `pets/all`...')
-    try {
-        const pets = await Pet.find()
-        res.status(200).json({
-            status: 200,
-            message: HTTPMessagesEnum.PETS_FETCHED,
-            data: pets,
-        })
-    } catch (error) {
-        res.status(400).json({
-            status: 400,
-            message: error,
-        })
-    } finally {
-        console.log('GET: pets/all completed')
-    }
+//     console.log('Fetching pets from `pets/all`...')
+//     try {
+//         const pets = await Pet.find()
+//         res.status(200).json({
+//             status: 200,
+//             message: HTTPMessagesEnum.PETS_FETCHED,
+//             data: pets,
+//         })
+//     } catch (error) {
+//         res.status(400).json({
+//             status: 400,
+//             message: error,
+//         })
+//     } finally {
+//         console.log('GET: pets/all completed')
+//     }
+res.status(200).json({
+        status: 200,
+        message: HTTPMessagesEnum.PETS_FETCHED,
+        data: []
+})
 })
 
 module.exports = router
